6d08cc8d0e5f21e73b5fe824d6ebd889d0499ca0
NicoRizz16 <nicolas.rizzon@gmail.com>
2017-05-08 12:59:58 +0200

    Upload fichiers taxref et taxreflink ajouté
    Signed-off-by: NicoRizz16 <nicolas.rizzon@gmail.com>

--- src/AppBundle/Controller/TaxrefController.php
+++ src/AppBundle/Controller/TaxrefController.php
@@ -1,6 +1,10 @@
 <?php
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\TaxrefFile;
+use AppBundle\Entity\TaxrefLinkFile;
+use AppBundle\Form\TaxrefFileType;
+use AppBundle\Form\TaxrefLinkFileType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -8,11 +12,72 @@ use Symfony\Component\HttpFoundation\Request;
 class TaxrefController extends Controller
 {
     /**
-     * @Route("/taxref", name="app_taxref")
+     * @Route("/taxref/charger", name="upload_taxref")
      *
      */
-    public function taxrefAction()
+    public function showUploadFormsAction()
     {
-        return $this->render('AppBundle:MainController:index.html.twig');
+        $taxrefFile = new TaxrefFile();
+        $taxrefForm = $this->createForm(TaxrefFileType::class, $taxrefFile, array(
+            'action' => $this->generateUrl('upload_taxref_taxref')
+        ));
+
+        $taxrefLinkFile = new TaxrefLinkFile();
+        $taxrefLinkForm = $this->createForm(TaxrefLinkFileType::class, $taxrefLinkFile, array(
+            'action' => $this->generateUrl('upload_taxref_taxreflink')
+        ));
+
+        return $this->render('admin/taxref/upload.html.twig', array(
+            'taxrefForm' => $taxrefForm->createView(),
+            'taxrefLinkForm' => $taxrefLinkForm->createView()
+        ));
+    }
+
+    /**
+     * @Route("/taxref/charger/taxref", name="upload_taxref_taxref")
+     */
+    public function uploadTaxrefAction(Request $request)
+    {
+        $taxrefFile = new TaxrefFile();
+        $taxrefForm = $this->createForm(TaxrefFileType::class, $taxrefFile, array(
+            'action' => $this->generateUrl('upload_taxref_taxref')
+        ));
+
+        $taxrefForm->handleRequest($request);
+        if ($taxrefForm->isSubmitted() && $taxrefForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($taxrefFile);
+            $em->flush();
+
+            $this->addFlash('info', 'La table taxref a bien été téléchargée.');
+            return $this->redirectToRoute('upload_taxref');
+        }
+
+        $this->addFlash('error', 'La table taxref n\'a pas été téléchargée.');
+        return $this->redirectToRoute('upload_taxref');
+    }
+
+    /**
+     * @Route("/taxref/charger/taxreflink", name="upload_taxref_taxreflink")
+     */
+    public function uploadTaxreflinkAction(Request $request)
+    {
+        $taxrefLinkFile = new TaxrefLinkFile();
+        $taxrefLinkForm = $this->createForm(TaxrefLinkFileType::class, $taxrefLinkFile, array(
+            'action' => $this->generateUrl('upload_taxref_taxreflink')
+        ));
+
+        $taxrefLinkForm->handleRequest($request);
+        if ($taxrefLinkForm->isSubmitted() && $taxrefLinkForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($taxrefLinkFile);
+            $em->flush();
+
+            $this->addFlash('info', 'La table taxref_link a bien été téléchargée.');
+            return $this->redirectToRoute('upload_taxref');
+        }
+
+        $this->addFlash('error', 'La table taxref_link n\'a pas été téléchargée.');
+        return $this->redirectToRoute('upload_taxref');
     }
 }
