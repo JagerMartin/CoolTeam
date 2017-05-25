eb77ad9e735a3401bcbfad896978bed2efc6e010
NicoRizz16 <nicolas.rizzon@gmail.com>
2017-05-25 20:02:18 +0200

    Entité User complétée
    Signed-off-by: NicoRizz16 <nicolas.rizzon@gmail.com>

--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -5,10 +5,13 @@ namespace AppBundle\Entity;
 
 use FOS\UserBundle\Model\User as BaseUser;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
 
 /**
  * @ORM\Entity(repositoryClass="AppBundle\Repository\UserRepository")
  * @ORM\Table(name="fos_user")
+ * @Vich\Uploadable
  */
 class User extends BaseUser
 {
@@ -49,12 +52,81 @@ class User extends BaseUser
      */
     private $dateOfBirth;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="organization", type="string", length=255, nullable=true)
+     */
+    private $organization;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="observation_frequency", type="string", length=255, nullable=true)
+     */
+    private $observationFrequency;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="speciality", type="string", length=255, nullable=true)
+     */
+    private $speciality;
+
+    /**
+    * NOTE: This is not a mapped field of entity metadata, just a simple property.
+    *
+    * @Vich\UploadableField(mapping="user_profile_photo", fileNameProperty="imageName")
+    *
+    * @var File
+    */
+    private $imageFile;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     *
+     * @var string
+     */
+    private $imageName;
+
+
+    /**
+     * @ORM\Column(type="datetime")
+     *
+     * @var \DateTime
+     */
+    private $updatedAt;
+
 
 
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+    /**
+     * If manually uploading a file (i.e. not using Symfony Form) ensure an instance
+     * of 'UploadedFile' is injected into this setter to trigger the  update. If this
+     * bundle's configuration parameter 'inject_on_load' is set to 'true' this setter
+     * must be able to accept an instance of 'File' as the bundle will inject one here
+     * during Doctrine hydration.
+     *
+     * @param File|\Symfony\Component\HttpFoundation\File\UploadedFile $image
+     *
+     * @return User
+     */
+    public function setImageFile(File $image = null)
+    {
+        $this->imageFile = $image;
+
+        if ($image) {
+            // It is required that at least one field changes if you are using doctrine
+            // otherwise the event listeners won't be called and the file is lost
+            $this->updatedAt = new \DateTimeImmutable();
+        }
+
+        return $this;
     }
 
     /**
@@ -151,5 +223,125 @@ class User extends BaseUser
     public function getDateOfBirth()
     {
         return $this->dateOfBirth;
+    }
+
+    /**
+     * Set organization
+     *
+     * @param string $organization
+     *
+     * @return User
+     */
+    public function setOrganization($organization)
+    {
+        $this->organization = $organization;
+
+        return $this;
+    }
+
+    /**
+     * Get organization
+     *
+     * @return string
+     */
+    public function getOrganization()
+    {
+        return $this->organization;
+    }
+
+    /**
+     * Set observationFrequency
+     *
+     * @param string $observationFrequency
+     *
+     * @return User
+     */
+    public function setObservationFrequency($observationFrequency)
+    {
+        $this->observationFrequency = $observationFrequency;
+
+        return $this;
+    }
+
+    /**
+     * Get observationFrequency
+     *
+     * @return string
+     */
+    public function getObservationFrequency()
+    {
+        return $this->observationFrequency;
+    }
+
+    /**
+     * Set speciality
+     *
+     * @param string $speciality
+     *
+     * @return User
+     */
+    public function setSpeciality($speciality)
+    {
+        $this->speciality = $speciality;
+
+        return $this;
+    }
+
+    /**
+     * Get speciality
+     *
+     * @return string
+     */
+    public function getSpeciality()
+    {
+        return $this->speciality;
+    }
+
+    /**
+     * Set imageName
+     *
+     * @param string $imageName
+     *
+     * @return User
+     */
+    public function setImageName($imageName)
+    {
+        $this->imageName = $imageName;
+
+        return $this;
+    }
+
+    /**
+     * Get imageName
+     *
+     * @return string
+     */
+    public function getImageName()
+    {
+        return $this->imageName;
+    }
+
+    /**
+     * Set updatedAt
+     *
+     * @param \DateTime $updatedAt
+     *
+     * @return User
+     */
+    public function setUpdatedAt($updatedAt)
+    {
+        $this->updatedAt = $updatedAt;
+
+        return $this;
+    }
+
+    /**
+     * Get updatedAt
+     *
+     * @return \DateTime
+     */
+    public function getUpdatedAt()
+    {
+        return $this->updatedAt;
     }
 }
