55f4ba182ae7cd653c8e5735bf06f8ea0a174d96
Atlantis11 <atlantis11@libertysurf.fr>
2017-05-31 10:44:03 +0200

    réparation bugs d'affichage sur la homepage + javascript

--- web/js/main.js
+++ web/js/main.js
@@ -65,3 +65,70 @@ Waves.attach('.btn, .btn-floating', ['waves-light']);
 Waves.attach('.img-waves a', ['waves-light']);
 Waves.attach('.navbar a', ['waves-light']);
 Waves.init();
+
+/**************************
+ * Pour la homepage     **
+ *************************/
+//Carousel Header
+$('#myCarousel1, #myCarousel2').carousel({
+    interval: 10000
+});
+//Animation cercle
+new WOW().init();
+/**********/
+/*compteur*/
+/*********/
+$(document).ready(function(){
+    $(".bhide").click(function(){
+        $(".hideObj").slideDown();
+        $(this).hide(); //.attr()
+        return false;
+    });
+    $(".bhide2").click(function(){
+        $(".container.hideObj2").slideDown();
+        $(this).hide(); // .attr()
+        return false;
+    });
+    $('.heart').mouseover(function(){
+        $(this).find('i').removeClass('fa-heart-o').addClass('fa-heart');
+    }).mouseout(function(){
+        $(this).find('i').removeClass('fa-heart').addClass('fa-heart-o');
+    });
+    function sdf_FTS(_number,_decimal,_separator)
+    {
+        var decimal=(typeof(_decimal)!='undefined')?_decimal:2;
+        var separator=(typeof(_separator)!='undefined')?_separator:'';
+        var r=parseFloat(_number)
+        var exp10=Math.pow(10,decimal);
+        r=Math.round(r*exp10)/exp10;
+        rr=Number(r).toFixed(decimal).toString().split('.');
+        b=rr[0].replace(/(\d{1,3}(?=(\d{3})+(?:\.\d|\b)))/g,"\$1"+separator);
+        r=(rr[1]?b+'.'+rr[1]:b);
+        return r;
+    }
+    setTimeout(function(){
+        $('#counter').text('0');
+        $('#counter1').text('0');
+        $('#counter2').text('0');
+        $('#counter3').text('0');
+        setInterval(function(){
+            var curval=parseInt($('#counter').text());
+            var curval1=parseInt($('#counter1').text().replace(' ',''));
+            var curval2=parseInt($('#counter2').text());
+            var curval3=parseInt($('#counter3').text());
+            if(curval<=200-1){
+                $('#counter').text(curval+1);
+            }
+            if(curval1<=150-1){
+                $('#counter1').text(curval1+1);
+            }
+            if(curval2<=3000-10){
+                $('#counter2').text(curval2+10);
+            }
+            if(curval3<=245-1){
+                $('#counter3').text(curval3+1);
+            }
+        }, 2);
+    }, 500);
+});
+
